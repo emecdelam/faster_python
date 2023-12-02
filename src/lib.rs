1f38cd76a84387f52430662741dd5f3c9bf5d45c
Emec Delam <kiwi.bp.surviv@gmail.com>
2023-12-02 23:04:56 +0100

    test

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,13 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
+#[pyo3(signature = (a=0,b=0))]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+#[pyfunction]
+#[pyo3(signature = (a=0,b=0))]
+fn sum_as_list(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
@@ -10,5 +16,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn faster_python(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_as_list, m)?)?;
     Ok(())
 }
